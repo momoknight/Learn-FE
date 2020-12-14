9ec5f5b096748f90c7bd8eeb7bafa45960dfed9a
momoknight <yincheng.tao@outlook.com>
2020-12-14 21:07:45 +0800

    删除了两个命名错误的文件

--- PTA/t1/p2-2.js
+++
@@ -1,9 +0,0 @@
-for(let i = 0; i < 4; i++) {
-    for(let j = 0; j < i; j++) {
-        document.write('&nbsp');
-    }
-    for(let k = 0; k < 4 - i; k++) {
-        document.write('* ');
-    }
-    document.write('<br/>');
-}
--- PTA/t1/p3-1.js
+++
@@ -1,4 +0,0 @@
-let fahr = 150;
-// 要求输出为整型
-let celsius = parseInt(5 * (fahr - 32) / 9);
-console.log(`fahr = ${fahr}, celsius = ${celsius}`);
