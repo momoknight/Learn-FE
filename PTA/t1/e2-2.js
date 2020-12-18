84195d532eadb2941dcf1a3d7a8efb50e3f9fc5a
momoknight <yincheng.tao@outlook.com>
2020-12-18 22:07:04 +0800

    修改习题2-2代码

--- PTA/t1/e2-2.js
+++ PTA/t1/e2-2.js
@@ -3,15 +3,17 @@ let chargeBtn = document.querySelector('#charge');
 chargeBtn.addEventListener('click', function() {
     let power = parseFloat(document.querySelector('#power').value);
     let result = document.querySelector('#result12');
-    let cost = 0;
+    let cost;
     
-    if (cost < 0) {
+    if (power < 0) {
         cost = 'Invalid Value!';
     } else if (power <= 50) {
         cost = 0.53 * power; 
+        cost = cost.toFixed(2);
     } else {
         cost = 0.53 * power + 0.05 * (power - 50);
+        cost = cost.toFixed(2);
     }
 
-    result.innerHTML = `cost = ${cost.toFixed(2)}`;
+    result.innerHTML = `cost = ${cost}`;
 })
