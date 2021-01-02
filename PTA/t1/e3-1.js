7a4e6e9df508b0d92039b98562eacfe92e5c2eed
momoknight <yincheng.tao@outlook.com>
2021-01-02 22:04:38 +0800

    习题3-1 比较大小

---
+++ PTA/t1/e3-1.js
@@ -0,0 +1,31 @@
+let btn1 = document.querySelector('#btn1');
+
+
+
+btn1.addEventListener('click', function () {
+  let a = parseInt(document.querySelector('#a').value);
+  let b = parseInt(document.querySelector('#b').value);
+  let c = parseInt(document.querySelector('#c').value);
+  let result = document.querySelector('#result1');
+
+  let min;
+  if (a > b) {
+    min = b;
+    b = a;
+    a = min;
+  }
+
+  if (a > c) {
+    min = c;
+    c = a;
+    a = min;
+  }
+
+  if (b > c) {
+    min = c;
+    c = b;
+    b = min;
+  }
+  
+  result.innerHTML = `${a}->${b}->${c}`;
+})
