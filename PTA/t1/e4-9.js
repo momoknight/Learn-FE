f949da03782f8c7aeabe84c336244a99e815cdc9
momoknight <yincheng.tao@outlook.com>
2021-01-18 22:19:50 +0800

    习题4-9 打印菱形

---
+++ PTA/t1/e4-9.js
@@ -0,0 +1,30 @@
+let btn9 = document.querySelector('#btn9');
+
+btn9.addEventListener('click', function () {
+  let h = parseInt(document.querySelector('#h').value);
+  let n = parseInt(document.querySelector('#n9').value);
+  let result = document.querySelector('#result9');
+  let resultStr = '<br>';
+
+  for (let i = 0; i < (n + 1) / 2; i++) {
+    for (let j = (n + 1) / 2 - 1; j > i; j--) {
+      resultStr += '&nbsp;';
+    }
+    for (let k = 0; k < 2 * i + 1; k++) {
+      resultStr += '* ';
+    }
+    resultStr += '<br>';
+  }
+
+  for (let i = (n + 1) / 2; i < n; i++) {
+    for (let j = (n + 1) / 2; j <= i; j++) {
+      resultStr += '&nbsp;';
+    }
+    for (let k = 2 * (n - i - 1) + 1; k > 0; k--) {
+      resultStr += '* ';
+    }
+    resultStr += '<br>';
+  }
+
+  result.innerHTML = resultStr;
+})
