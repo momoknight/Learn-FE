01e3253289ece1321173c37f48944c9018d3dab5
momoknight <yincheng.tao@outlook.com>
2021-02-06 22:49:58 +0800

    习题6-7 简单计算器

---
+++ PTA/t1/p6-7.js
@@ -0,0 +1,51 @@
+let btn7 = document.querySelector('#btn7');
+
+btn7.addEventListener('click', function () {
+  let operation, a, b, input, z;
+  let result = document.querySelector('#result7');
+  let resultValue = 0;
+
+  a = parseInt(prompt('Please enter a number or an operator: '));
+  operation = a;
+  // if (!isNaN(input)) {
+  //   input = parseInt(input);
+  // }
+  input = prompt('Please enter a number or an operator: ');
+  while (input != null) {
+    switch (input) {
+      case '+':
+        b = parseInt(prompt('Please enter a number or an operator: '));
+        operation += b;
+        break;
+      case '-':
+        b = parseInt(prompt('Please enter a number or an operator: '));
+        operation -= b;
+        break;
+      case '*':
+        b = parseInt(prompt('Please enter a number or an operator: '));
+        operation *= b;
+        break;
+      case '/':
+        b = parseInt(prompt('Please enter a number or an operator: '));
+        if (b == 0) {
+          resultValue = 'ERROR';
+          z = 0;
+          break;
+        } else {
+          operation /= b;
+          break;
+        }
+      case '=':
+        resultValue = operation;
+        z = 0;
+        break;
+    }
+
+    if (z == 0) {
+      break;
+    }
+    input = prompt('Please enter a number or an operator: ');
+  }
+
+  result.innerHTML = `${resultValue}`;
+})
